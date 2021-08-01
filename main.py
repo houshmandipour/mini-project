641ef4dd193d0b5943fe0ce1ec14f4be2b94c743
Ali Houshmandi pour <88240069+houshmandipour@users.noreply.github.com>
2021-08-01 10:12:58 +0430

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ def main():
 
 def play_loop():
     global play_game
-    # play_game = input("Do You want to play again? y = yes, n = no \n")
     while play_game not in ["y", "n","Y","N"]:
         play_game = input("Do You want to play again? y = yes, n = no \n")
     if play_game == "y" or "Y":
