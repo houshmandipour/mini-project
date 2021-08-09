1b54edd8a53ee310f86dc202129b48f378b7a0f0
Ali Houshmandi pour <88240069+houshmandipour@users.noreply.github.com>
2021-08-09 23:12:51 +0430

    Add files via upload

---
+++ internet_speed_test.py
@@ -0,0 +1,11 @@
+from cgi import print_environ
+import speedtest
+
+test = speedtest.Speedtest()
+download = test.download()
+upload = test.upload()
+
+print(download)
+print(upload)
+print(f"Download speed : {download/8000000} MBps ")
+print(f"Upload speed : {upload/8000000} MBps")
